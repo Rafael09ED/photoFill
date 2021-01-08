f0bf24147b1e91d9b089a66c95a524d2b93ebab3
Rafael09ED <starmarstrek@gmail.com>
2021-01-08 03:26:49 -0500

    enabled scaling

--- src/index.ts
+++ src/index.ts
@@ -485,7 +485,7 @@ function getSizeGivenSpace(old: Rectangle, points : Point[]) : Rectangle {
     
     const dir = getOutsideDirectionOfLine(points[1], points[2]);
     const lineLength = lenghtOfLine(points[1], points[2]);
-    if (true) { //TODO: this section is cheating, find better solution
+    if (false) { //TODO: this section is cheating, find better solution
         const boxHeight = Math.min(lenghtOfLine(points[0], points[1]), lenghtOfLine(points[2], points[3]));
         switch(dir){
             case CardinalDirection.SOUTH: 
